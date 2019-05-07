b19ebaf1d127198ee9500130062a123000cbf9c8
Kent Inverarity <None>
2019-05-07 16:07:08 +0930

    Add markdown for PyPI

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ setup(
     name="hydllp-server",
     version="0.1.0",
     description="CherryPy server for the Hydstra HYDLLPX executable",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/kinverarity1/hydllp-server",
     author="Kent Inverarity",
     author_email="kent.inverarity@sa.gov.au",
     packages=["hydllp_server"],
     install_requires=(),
-    entry_points={"console_scripts": ["hydllp-server = hydllp_server.webapp:run_entry_point"]},
+    entry_points={
+        "console_scripts": ["hydllp-server = hydllp_server.webapp:run_entry_point"]
+    },
 )
